5569bdc54ff2779351d31f19902ee7be9ca9f3ce
Sudan Bhandari <sudan1bhandari@gmail.com>
2025-08-19 15:42:22 +0545

    feat(ci): integrate ci

--- main/graphql/schema.py
+++ main/graphql/schema.py
@@ -4,8 +4,6 @@ from strawberry.django.views import AsyncGraphQLView
 from strawberry.file_uploads import Upload
 from strawberry_django.optimizer import DjangoOptimizerExtension
 
-
-
 from .context import GraphQLContext
 from .dataloaders import GlobalDataLoader
 from .enums import AppEnumCollection, AppEnumCollectionData
@@ -21,14 +19,15 @@ class CustomAsyncGraphQLView(AsyncGraphQLView):
 
 
 @strawberry.type
-class Query():
+class Query:
     enums: AppEnumCollection = strawberry.field(  # type: ignore[reportGeneralTypeIssues]
         resolver=lambda: AppEnumCollectionData(),
     )
 
+
 # NOTE: for now we are not using mutation
 @strawberry.type
-class Mutation(): ...
+class Mutation: ...
 
 
 schema = strawberry.Schema(
