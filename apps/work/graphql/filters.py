3bfdde4d6964830edbc177e1d22f409ed23e3d1a
subinasr <ssr.subina@gmail.com>
2025-08-30 14:45:51 +0545

    fix(filter): remove unwanted filters in works query

--- apps/work/graphql/filters.py
+++ apps/work/graphql/filters.py
@@ -6,6 +6,4 @@ from apps.work.models import Work
 
 @strawberry_django.filters.filter(Work, lookups=True)
 class WorkFilter:
-    id: strawberry.ID
-    department: strawberry.ID
     strategic_directive: strawberry.ID
